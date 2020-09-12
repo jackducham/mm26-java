f7501a8ba2088f06736836d609f770f86a3b06ef
Fgoyal <fgoyal2@illinois.edu>
2020-09-12 17:04:44 -0400

    Created helper function for PlayerDecision

--- src/main/java/mech/mania/starter_pack/domain/PlayerStrategy.java
+++ src/main/java/mech/mania/starter_pack/domain/PlayerStrategy.java
@@ -4,16 +4,21 @@ import mech.mania.engine.domain.model.CharacterProtos.*;
 import mech.mania.engine.domain.model.GameStateProtos.*;
 import mech.mania.engine.domain.model.PlayerProtos.*;
 
-public class PlayerStrategy implements Strategy{
+public class PlayerStrategy implements Strategy {
+    public PlayerDecision createPlayerDecision(DecisionType decision, int x, int y, String boardId, int index) {
+        return PlayerDecision.newBuilder()
+                .setDecisionType(decision)
+                .setTargetPosition(Position.newBuilder()
+                        .setX(x).setY(y).setBoardId(boardId)
+                        .build())
+                .setIndex(index)
+                .build();
+    }
+
     public PlayerDecision makeDecision(String playerName, GameState gameState){
         Player myPlayer = gameState.getPlayerNamesMap().get(playerName);
 
         // Returns MOVE decision to (0, 0) on private board
-        return PlayerDecision.newBuilder()
-                .setDecisionType(DecisionType.MOVE)
-                .setTargetPosition(Position.newBuilder()
-                        .setX(0).setY(0).setBoardId(playerName)
-                        .build())
-                .build();
+        return createPlayerDecision(DecisionType.MOVE, 0, 0, playerName,0);
     }
 }
